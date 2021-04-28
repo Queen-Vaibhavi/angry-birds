d62b74050330da856a9233cc72fdd963485b3176
Vaibhavi <princessvaibhavi@gmail.com>
2021-04-28 19:27:51 +0530

    I have made the xposi- of platform ,150 to 180

--- sketch.js
+++ sketch.js
@@ -13,11 +13,11 @@ function preload() {
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
-    world = engine.world;
+    world = engine.world;//vesu,rudraa
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform = new Ground(200, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
